baa86857ef34e45a9036dd4d88d7e6c377bec447
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-07-13 14:16:23 -0600

    Grabs correct building via param

--- app/controllers/building_controller.rb
+++ app/controllers/building_controller.rb
@@ -2,4 +2,11 @@ class BuildingController < ApplicationController
   def index
     @buildings = Building.all
   end
+
+  def single
+    p "bob"
+    p params[:id]
+    @single_building = Building.find(params[:id])
+
+  end
 end
