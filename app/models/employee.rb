cd7eb4467bb84466bed5991001a8420dbebd107c
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-07-13 23:20:42 -0600

    Added validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ApplicationRecord
   belongs_to :company
+
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 3 }
 end
