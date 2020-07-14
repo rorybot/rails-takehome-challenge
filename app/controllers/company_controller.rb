ecb59fa1077dc161d9a7747b5397e9e5e855f004
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-07-13 19:47:16 -0600

    Groups floors by building

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -2,8 +2,24 @@ class CompanyController < ApplicationController
   def single
     @single_company = Company.find(params[:id])
     @offices = @single_company.offices.map do |office|
-      {:building_id => office.building.id, :building_name => office.building.name, :floor_number => office.floor_number}
+      [[office.building_id,office.building.name], office.floor_number]
     end
+    @offices = @offices.group_by(&:first).map{ |key, value|
+      {:building_id => key[0], :building_name => key[1], :floors => [*value.map(&:last)]}
+    }
+
+
+        p @offices
+        p "foo"
+
+
+
+        # @offices = @single_company.offices.each do |office|
+        #   building_floors[:building_name] = office.building.name
+        # end
+        # @offices = building_floors
+
+
   end
 
 end
