cec57f00f04e3d02880a4a47de4ecfadc5bdf965
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-07-14 06:42:10 -0600

    Removed print statement

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module NcarTakehome
   class Application < Rails::Application
-    p "Foobar"
-    p ENV
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
