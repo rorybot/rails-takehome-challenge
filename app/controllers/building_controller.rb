fda729b3975743c6b60e5071728bdae57c1fc066
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-07-13 18:19:29 -0600

    Cleaned up; proper format of array

--- app/controllers/building_controller.rb
+++ app/controllers/building_controller.rb
@@ -5,12 +5,8 @@ class BuildingController < ApplicationController
 
   def single
     @single_building = Building.find(params[:id])
-    @offices = @single_building.offices
-    p "foo"
-    @mapped = @single_building.offices.map do |office|
-      {:floor_number => office.floor_number, :company => office.company.name}
+    @offices = @single_building.offices.map do |office|
+      {:floor_number => office.floor_number, :company_name => office.company.name}
     end
-
-
   end
 end
