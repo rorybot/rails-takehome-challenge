44aeee7a2243429bfafe669cd425a18696d75e2c
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-07-13 18:20:04 -0600

    Connect to single company

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   get 'building/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # root 'building#index'
-
-
   get 'building/:id', to: 'building#single'
+  get 'company/:id', to: 'company#single'
 end
